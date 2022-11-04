179431f83cb6e9ec9592834b561054222b55b89a
Dawid Popiolek <dawidpopiolek@protonmail.com>
2022-11-04 16:04:19 +0100

    Implemented using setTimeout

--- main.js
+++ main.js
@@ -12,3 +12,19 @@ const aliceTiming = {
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
+
+function doStep1() {
+  alice1.animate(aliceTumbling, aliceTiming);
+}
+
+function doStep2() {
+  alice2.animate(aliceTumbling, aliceTiming);
+}
+
+function doStep3() {
+  alice3.animate(aliceTumbling, aliceTiming);
+}
+
+doStep1();
+setTimeout(doStep2, aliceTiming.duration);
+setTimeout(doStep3, aliceTiming.duration*2);
